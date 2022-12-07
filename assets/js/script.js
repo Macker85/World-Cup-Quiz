599220cef1ee9bbb424f31c12f066337f9268186
Macker85 <pmacker1985@gmail.com>
2022-12-07 21:16:42 +0000

    Add layout for potential functions needed

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+function runGame(){
+
+}
+
+function checkAnswer() {
+    
+}
+
+function calculateCorrectAnswer() {
+    
+}
+
+function incrementScore() {
+    
+}
+
+function incrementWrongAnswer() {
+    
+}
+
+function displayQuestion() {
+    
+}
+
+function displayAnswerA() {
+    
+}
+
+function displayAnswerB() {
+    
+}
+
+function displayAnswerC() {
+    
+}
+
+function displayAnswerD() {
+    
+}
