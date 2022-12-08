46d09d14bba0444c5ca8ecb293c4515eba685639
Macker85 <pmacker1985@gmail.com>
2022-12-08 18:43:25 +0000

    Add JS file and questions for quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
+const startButton = document.getElementById('start-btn');
+
+startButton.addEventListener('click', startGame);
+
+function startGame(){
+    console.log('started');
+}
+
 function runGame(){
 
 }
